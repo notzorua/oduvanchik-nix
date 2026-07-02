6e550243f047163a65906536a9c16cd07c2f7d53
adarkaz <swdmeow@gmail.com>
2026-07-02 23:29:46 +0300

    add chromium for WebHID-based device configurators

---
+++ modules/home/vscode.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      golang.go
+    ];
+  };
+
+  home.packages = with pkgs; [
+    pipes-rs
+    cbonsai     
+    cmatrix     
+    samrewritten
+    asciiquarium-transparent
+    protontricks
+    chromium
+  ];
+}

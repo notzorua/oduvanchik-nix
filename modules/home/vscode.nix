fee8b57ebaac7cfe4e139c97626b74bdf2c14202
notzorua <zzzzoroa@gmail.com>
2026-07-11 23:13:57 +0300

    remove some models

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -2,9 +2,6 @@
   pkgs,
   ...
 }:
-let
-  ollamaCuda = pkgs.ollama.override { acceleration = "cuda"; };
-in
 {
   programs.vscode = {
     enable = true;
@@ -27,15 +24,5 @@ in
     virt-manager
     tesseract
     cool-retro-term
-    ollamaCuda
   ];
-
-  systemd.user.services.ollama = {
-    Unit.Description = "Ollama server";
-    Install.WantedBy = [ "default.target" ];
-    Service = {
-      ExecStart = "${ollamaCuda}/bin/ollama serve";
-      Restart = "on-failure";
-    };
-  };
 }

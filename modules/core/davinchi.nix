1a67da0a1decbf6c61b9f1a819c46c65c29b5f7d
notzorua <zzzzoroa@gmail.com>
2026-09-22 20:06:43 +0300

    davinchi removed, dotnet for vscode added

--- modules/core/davinchi.nix
+++ modules/core/davinchi.nix
@@ -6,6 +6,6 @@
 }:
 {
   environment.systemPackages = with pkgs; [
-    davinci-resolve
+#    davinci-resolve
   ];
 }
--- modules/home/vscodium.nix
+++ modules/home/vscodium.nix
@@ -4,6 +4,8 @@
     enable = true;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       golang.go
+      ms-dotnettools.csharp
+      ms-dotnettools.vscode-dotnet-runtime
     ];
   };
 }
